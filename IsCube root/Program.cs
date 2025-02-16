db725766ab93ea94f1662c711c530ec52379dab0
Lebohang mofokeng <105319206+LebohangLuuh@users.noreply.github.com>
2025-02-16 13:21:33 +0200

    is cube root ??

---
+++ IsCube root/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        if (int.TryParse(Console.ReadLine(), out int power))
+        {
+            Console.WriteLine(IsMagicalPotion(power) ? "YES" : "NO");
+        }
+    }
+
+    static bool IsMagicalPotion(int power)
+    {
+        if (power < 1) return false;
+        
+        int cubeRoot = (int)Math.Round(Math.Pow(power, 1.0 / 3.0));
+        return cubeRoot * cubeRoot * cubeRoot == power;
+    }
+}
